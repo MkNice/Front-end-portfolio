81b21c3a38f264e9a0b624977eba6dcdc30a58c5
MkNice <mk1hitch@gmail.com>
2025-03-21 07:25:14 +0200

    add firebase, ngprime, update tsconfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
+import { providePrimeNG } from 'primeng/config';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    providePrimeNG()
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCFq7k3TR_rxueg37tRDfkD24s_Rfpf3M8",
+        authDomain: "portfolio-16a16.firebaseapp.com",
+        projectId: "portfolio-16a16",
+        storageBucket: "portfolio-16a16.firebasestorage.app",
+        messagingSenderId: "904084944657",
+        appId: "1:904084944657:web:c416824a60062854cafeb0"
+    }
+};
