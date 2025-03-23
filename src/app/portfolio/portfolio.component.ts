1c3c0d799d167d717223256328459c6c59c4df8d
MkNice <mk1hitch@gmail.com>
2025-03-23 16:46:12 +0200

    add layout

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IntroComponent } from "./intro/intro.component";
 import { HeaderComponent } from "./header/header.component";
+import { TechStackComponent } from "./tech-stack/tech-stack.component";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [IntroComponent, HeaderComponent],
+  imports: [IntroComponent, HeaderComponent, TechStackComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
---
+++ src/app/portfolio/tech-stack/tech-stack.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tech-stack',
+  imports: [],
+  templateUrl: './tech-stack.component.html',
+  styleUrl: './tech-stack.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TechStackComponent {
+
+}
