5d5fd0effa8fa0c4ca028ebcbc0fdfc03d7a7984
MkIINice <mk1hitch@gmail.com>
2025-04-11 08:30:35 +0300

    add component

---
+++ src/app/shared/UI/section/section.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  imports: [],
+  templateUrl: './section.component.html',
+  styleUrl: './section.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SectionComponent {
+
+}
