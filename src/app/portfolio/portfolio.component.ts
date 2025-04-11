418cca8916da16ec69544d0b29d87d4e7b2ae0be
MkIINice <mk1hitch@gmail.com>
2025-04-11 08:26:55 +0300

    add base footer

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,10 +3,11 @@ import { IntroComponent } from "./intro/intro.component";
 import { HeaderComponent } from "./header/header.component";
 import { TechStackComponent } from "./tech-stack/tech-stack.component";
 import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [IntroComponent, HeaderComponent, TechStackComponent, ProjectsComponent],
+  imports: [IntroComponent, HeaderComponent, TechStackComponent, ProjectsComponent, FooterComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
