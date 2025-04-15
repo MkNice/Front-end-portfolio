968ff8943a9581ba740ef4e6f08cf203b1000181
MkIINice <mk1hitch@gmail.com>
2025-04-15 13:13:54 +0300

    add section code example, some styles, images with codes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from "./header/header.component";
 import { TechStackComponent } from "./tech-stack/tech-stack.component";
 import { ProjectsComponent } from './projects/projects.component';
 import { FooterComponent } from "./footer/footer.component";
+import { CodeExamplesComponent } from "./code-examples/code-examples.component";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [IntroComponent, HeaderComponent, TechStackComponent, ProjectsComponent, FooterComponent],
+  imports: [IntroComponent, HeaderComponent, TechStackComponent, ProjectsComponent, FooterComponent, CodeExamplesComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
